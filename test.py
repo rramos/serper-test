3cf8802813f35fefbda389a87b61e872eaa5450c
Rui Ramos <rui.ms.ramos@gmail.com>
2024-06-13 09:45:20 +0100

    init repo

---
+++ test.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+url = "https://google.serper.dev/search"
+
+# Load environment variables from .env file
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+# Search query
+query = "apple inc"
+
+payload = json.dumps({
+  "q": query
+})
+headers = {
+  'X-API-KEY': api_key,
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
